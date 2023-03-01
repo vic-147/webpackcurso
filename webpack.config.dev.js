ef4efac0f666eca7abce9d7f6443efa02f77e3ac
vic-147 <dmkorps@gmail.com>
2023-03-01 13:31:02 -0300

    agregado json.map

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -13,6 +13,7 @@ module.exports = {
         assetModuleFilename: 'assets/images/[hash][ext][query]'
     },
     mode: 'development',
+    devtool: 'source-map',
     //watch: true,
     resolve: {
         extensions: ['.js'],
